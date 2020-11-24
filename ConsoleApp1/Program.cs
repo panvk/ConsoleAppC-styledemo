6555b5110fbdab9bb55b76178b6e264ea128594e
panvk679 <panvk679@outlook.com>
2020-11-24 17:28:50 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("第二行");
+            Console.Write("不换行,");
+            Console.Write("然后");
+            Console.WriteLine(12.3m); //用"M"或者"m"表示十进制
+        }
+    }
+}
+
+
+
+
+C# 不同与 javastipt 和python ,强调数据类型
+用于演示而不是计算的字词、短语或任何字母数字数据的 string 类型
+表示单个字母数字字符的 char 类型
+表示整数的 int 类型
+表示带小数的数字的十进制类型
+表示 true/false 值的 bool 类型
